1998e1e93a66ed1680807398e69a30a040159098
SairajD <81271820+SairajD@users.noreply.github.com>
2021-07-30 21:21:12 +0530

    https

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import allReducer from './Components/reduxStore/reducers/allReducer';
+import HttpsRedirect from 'react-https-redirect';
 
 const analyticaStore = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
-    <Provider store = {analyticaStore}>
-        <App />
-    </Provider>
+    <HttpsRedirect>
+        <Provider store = {analyticaStore}>
+            <App />
+        </Provider>
+    </HttpsRedirect>
     , document.getElementById('root'));
 
 reportWebVitals();
